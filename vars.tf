136a12cf9fd63bc9a0bbec6269b9aac9e1a8e5d2
pravin menghani <pravinmenghani@gmail.com>
2019-06-18 11:14:35 +0000

    added updated files

--- vars.tf
+++ vars.tf
@@ -14,10 +14,10 @@ variable "AMIS" {
 }
 
 variable "PATH_TO_PRIVATE_KEY" {
-  default = "mykey"
+  default = "mykey2"
 }
 variable "PATH_TO_PUBLIC_KEY" {
-  default = "mykey.pub"
+  default = "mykey2.pub"
 }
 variable "INSTANCE_USERNAME" {
   default = "ubuntu"
